8b7ec4f5d1569e6dd9c918d34b0885f445a3b3fc
hadriansilva-cps <hadrians746@gmail.com>
2025-04-28 04:33:05 -0300

    Avaliacao

--- HelpApp/HelpApp.Infra.Data/Repositories/ProductRepository.cs
+++ HelpApp/HelpApp.Infra.Data/Repositories/ProductRepository.cs
@@ -3,10 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HelpApp.Domain.Entities;
+using HelpApp.Domain.Interfaces;
+using HelpApp.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace HelpApp.Infra.Data.Repositories
 {
-    public class ProductRepository
+    public class ProductRepository : IProductRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product> GetById(int? id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        public async Task<Product> Create(Product product)
+        {
+
+            _context.Add(product);
+            await _context.SaveChangesAsync();
+            return product;
+
+        }
+
+        public async Task<Product> Update(Product product)
+        {
+            _context.Entry(product).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> Remove(Product product)
+        {
+            _context.Remove(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
     }
 }
